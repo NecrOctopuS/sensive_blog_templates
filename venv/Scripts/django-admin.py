ba312ba108362031ff0a5919f3f6632e2c1d5f56
NecrOctopuS <53893624+NecrOctopuS@users.noreply.github.com>
2019-11-22 13:54:40 +0300

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\1\PycharmProjects\Devman\Website Layout\sensive_blog_templates\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
