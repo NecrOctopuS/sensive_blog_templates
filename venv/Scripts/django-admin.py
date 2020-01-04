d5e4ddb0546b3b254fef12d0276044c3f316941a
NecrOctopuS <53893624+NecrOctopuS@users.noreply.github.com>
2020-01-04 22:30:33 +0300

    Удалил venv и idea

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\1\PycharmProjects\Devman\Website Layout\sensive_blog_templates\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
